f43b548464444d77d6094d4b105ef1e206c4c252
Mehmet <arteta_london@outlook.com>
2022-09-24 19:02:51 +0300

    resources

--- YARP.Gateway/Program.cs
+++ YARP.Gateway/Program.cs
@@ -16,8 +16,10 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+
 var app = builder.Build();
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
